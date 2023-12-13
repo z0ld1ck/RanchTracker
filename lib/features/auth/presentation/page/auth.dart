88373122e73462bfaf79df1f6bf24a3f8b99d763
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2023-12-13 15:50:33 +0600

    added get sms code button

--- lib/features/auth/presentation/page/auth.dart
+++ lib/features/auth/presentation/page/auth.dart
@@ -17,22 +17,23 @@ class _AuthPageState extends State<AuthPage> {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Color(0xffebedff),
       appBar: AppBar(),
       body: Stack(
         children: [
           Positioned(
-            bottom:460,
+            bottom: 580,
             left: 0,
             right: 0,
             child: Transform.scale(
-              scale:1.5,
+              scale: 1.5,
               child: Container(
                 height: 400,
                 width: size.width,
                 decoration: BoxDecoration(
                   color: Color(0xff1D297E),
-                  shape:BoxShape.circle,
+                  shape: BoxShape.circle,
                 ),
               ),
             ),
@@ -57,14 +58,34 @@ class _AuthPageState extends State<AuthPage> {
                 color: Colors.white,
               ),
               margin: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
-              height: 400,
+              height: 450,
               child: Column(
                 children: [
                   SizedBox(height: 25),
-                  Text(
-                    'Регистрация',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+                  Row(
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(85, 0, 0, 0),
+                        child: Text(
+                          'Регистрация',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 27, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(37, 0, 0, 0),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.close,
+                            size: 34,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                   Padding(
                     padding: const EdgeInsetsDirectional.fromSTEB(0, 12, 0, 8),
@@ -83,12 +104,12 @@ class _AuthPageState extends State<AuthPage> {
                       controller: _name,
                       maxLines: 1,
                       decoration: InputDecoration(
-                        labelText: 'Имя',
+                        labelText: 'ФИО',
                         labelStyle: TextStyle(
                           fontWeight: FontWeight.w200,
                           fontSize: 14,
                         ),
-                        hintText: 'Введите имя',
+                        hintText: 'Введите ФИО',
                       ),
                     ),
                   ),
@@ -106,13 +127,42 @@ class _AuthPageState extends State<AuthPage> {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Получить код подтверждения'),
+                    padding: const EdgeInsetsDirectional.fromSTEB(0, 16, 0, 0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          color: Color(0xffA8A8A8),
+                          width: 330,
+                          height: 60,
+                          child: Center(
+                            child: Text(
+                              'Получить код подтверждения',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(90, 0, 0, 0),
+                        child: Text('Eсть аккаунт?'),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text('Войти'),
+                      ),
+                    ],
+                  )
                 ],
               ),
             ),
