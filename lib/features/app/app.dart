11522ee0c32d005fce1481f04a2e59305caf9a31
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2023-12-20 13:15:40 +0600

    ..

--- lib/features/app/app.dart
+++ lib/features/app/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:malshy/core/navigation/nav_services.dart';
+import 'package:malshy/core/routes/global_routes.dart';
 import 'package:malshy/core/theme/theme.dart';
 import 'package:malshy/features/app/home_page.dart';
 import 'package:malshy/features/auth/presentation/page/auth.dart';
@@ -9,7 +9,6 @@ import 'package:malshy/features/auth/presentation/page/log_in_page.dart';
 import 'package:malshy/features/auth/presentation/page/password_page.dart';
 import 'package:malshy/features/auth/presentation/splash/splash_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import '../../l10n/l10n.dart';
 import '../auth/presentation/page/sms_code_auth.dart';
 
 class App extends StatelessWidget {
@@ -23,33 +22,27 @@ class App extends StatelessWidget {
       splitScreenMode: true,
       builder: (_, child) {
         return MaterialApp(
-          locale: const Locale('ru'),
-          supportedLocales: L10n.all,
-          localizationsDelegates:AppLocalizations.localizationsDelegates,
+          locale: const Locale('en'),
+          supportedLocales: AppLocalizations.supportedLocales,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
           debugShowCheckedModeBanner: false,
           theme: APP_THEME,
           navigatorKey: NavigationService.navigationKey,
           // onGenerateRoute: GlobalRoutes.routes,
           onGenerateRoute: (RouteSettings settings) {
-            // if (!sharedPreference.isAuthenticated) {
-            //   return MaterialPageRoute(
-            //     builder: (_) => Splash(),
-            //   );
-            // }
-
             switch (settings.name) {
-              case '/':
-                return MaterialPageRoute(builder: (_) => Splash());
-              case '/auth_screen':
-                return MaterialPageRoute(builder: (_) => AuthPage());
-              case '/login_page':
-                return MaterialPageRoute(builder: (_) => LogInPage());
-              case '/sms_code_page':
-                return MaterialPageRoute(builder: (_) => SmSCodeAuthPage());
-              case '/password':
-                return MaterialPageRoute(builder: (_) => PasswordPage());
-              case '/home':
+              case GlobalRoutes.home:
                 return MaterialPageRoute(builder: (_) => HomePage());
+              case GlobalRoutes.register:
+                return MaterialPageRoute(builder: (_) => AuthPage());
+              case GlobalRoutes.login:
+                return MaterialPageRoute(builder: (_) => LogInPage());
+              case GlobalRoutes.sms:
+                return MaterialPageRoute(builder: (_) => SmSCodeAuthPage());
+              case GlobalRoutes.password:
+                return MaterialPageRoute(builder: (_) => PasswordPage());
+              case GlobalRoutes.splash:
+                return MaterialPageRoute(builder: (_) => Splash());
               default:
                 return null;
             }
