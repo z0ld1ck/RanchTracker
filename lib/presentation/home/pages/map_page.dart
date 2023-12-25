19f94c49a45aec5832034599badab747df0cb55b
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2023-12-25 16:57:20 +0600

    map page was added

--- lib/presentation/home/pages/map_page.dart
+++ lib/presentation/home/pages/map_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:malshy/features/app/app.dart';
+import 'package:malshy/presentation/home/widgets/add_cattle_button.dart';
+import 'package:malshy/presentation/home/widgets/search_bar_widget.dart';
 
 import '../../../core/const/app_colors.dart';
-import '../../../core/const/app_icons.dart';
+import '../widgets/cattle_counting_button.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -16,11 +21,58 @@ class _MapPageState extends State<MapPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
-      body: Column(
-        children: const [
-          Center(
-            child: Text('Map'),
-          )
+      appBar: PreferredSize(
+        preferredSize: Size(double.infinity, 30.h),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+        ),
+      ),
+      body: Stack(
+        children: [
+          FlutterMap(
+            options: MapOptions(
+              center: LatLng(50.97181272767628, 71.32178189582423),
+              zoom: 17,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate:
+                    'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+                userAgentPackageName: 'com.example.app',
+              ),
+              PolygonLayer(
+                polygons: [
+                  Polygon(
+                    points: [
+                      LatLng(50.9727176196799, 71.32054145080292),
+                      LatLng(50.972062298127405, 71.32357771150144),
+                      LatLng(50.97150145172532, 71.32368497676917),
+                      LatLng(50.9702312146473, 71.32283736277056),
+                      LatLng(50.97099466934939, 71.31971531724152),
+                    ],
+                    color: AppColors.white.withOpacity(0.1),
+                    borderStrokeWidth: 2,
+                    borderColor: AppColors.white,
+                    isFilled: true,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(19, 16, 16, 0),
+            child: CustomSearchBar(),
+          ),
+          Positioned(
+            left:130,
+            right:125,
+            top:676,
+            bottom: 12,
+            child: CattleCountingButton(
+              onPressed: () {},
+              text: 'Пересчет скота',
+            ),
+          ),
         ],
       ),
     );
