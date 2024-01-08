4fb0c60a75821841a307a80a8f30fbfde9048c4d
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2024-01-08 20:57:13 +0600

    realization of data failure

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -1,8 +1,16 @@
-import 'package:equatable/equatable.dart';
+import 'package:dio/dio.dart';
 
-abstract class Failure extends Equatable {
-  const Failure();
+abstract class Failure<T> {
+  final T? data;
+  final DioException? error;
 
-  @override
-  List<Object> get props => [];
+  const Failure({this.data, this.error});
+}
+
+class DataSuccess<T> extends Failure<T> {
+  const DataSuccess(T data) : super(data: data);
+}
+
+class DataFailed<T> extends Failure<T> {
+  const DataFailed(DioException error) : super(error: error);
 }
