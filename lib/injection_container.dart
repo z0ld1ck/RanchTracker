7b0f0408aeef0e18045e594cb51ab243a5579735
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2024-01-15 12:21:52 +0600

    usecase registration

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -6,6 +6,8 @@ import 'package:malshy/features/auth/domain/usecases/register_usecase.dart';
 import 'package:malshy/features/livestock_list_page/data/repositories/live_stock_repository_impl.dart';
 import 'package:malshy/features/livestock_list_page/domain/repositories/live_stock_repository.dart';
 import 'package:malshy/features/livestock_list_page/domain/usecases/create_livestock_usecase.dart';
+import 'package:malshy/features/livestock_list_page/domain/usecases/get_addition_type_usecase.dart';
+import 'package:malshy/features/livestock_list_page/domain/usecases/get_types_and_breeds_usecase.dart';
 
 final sl = GetIt.instance;
 
@@ -22,4 +24,6 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<LoginUsecase>(LoginUsecase(sl()));
   sl.registerSingleton<RegisterUsecase>(RegisterUsecase(sl()));
   sl.registerSingleton<CreateLiveStockUseCase>(CreateLiveStockUseCase(sl()));
+  sl.registerSingleton<GetTypesAndBreedsUseCase>(GetTypesAndBreedsUseCase(sl()));
+  sl.registerSingleton<GetAdditionTypeUseCase>(GetAdditionTypeUseCase(sl()));
 }
