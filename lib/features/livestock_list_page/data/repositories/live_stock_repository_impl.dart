a18e105beee321fe3bc8dec3a67a36452ef7b02b
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2024-01-15 12:20:55 +0600

    realization of repository

--- lib/features/livestock_list_page/data/repositories/live_stock_repository_impl.dart
+++ lib/features/livestock_list_page/data/repositories/live_stock_repository_impl.dart
@@ -6,11 +6,14 @@ import 'package:malshy/core/network/api_endpoints.dart';
 import 'package:malshy/core/network/custom_exceptions.dart';
 import 'package:malshy/core/network/network_client.dart';
 import 'package:malshy/core/utils/data_state.dart';
+import 'package:malshy/features/livestock_list_page/data/models/addition_type_model.dart';
+import 'package:malshy/features/livestock_list_page/data/models/type_breeds_model.dart';
 import '../../domain/repositories/live_stock_repository.dart';
 import '../models/livestock_model.dart';
 
 class LiveStockRepositoryImpl implements LiveStockRepository {
   final NetworkClient _networkClient = GetIt.I.get<NetworkClient>();
+
   @override
   Future<DataState<LivestockModel>> createLiveStock(
       LivestockModel livestockModel) async {
@@ -34,7 +37,6 @@ class LiveStockRepositoryImpl implements LiveStockRepository {
         ),
       ) as Response;
 
-
       if (response.statusCode == HttpStatus.created) {
         LivestockModel livestockModel = LivestockModel.fromJson(
           jsonDecode(response.body),
@@ -43,10 +45,87 @@ class LiveStockRepositoryImpl implements LiveStockRepository {
         return DataSuccess(livestockModel);
       } else {
         return DataFailed(
-            CustomException(message: 'bad response'));
+          CustomException(message: 'bad response'),
+        );
       }
     } on CustomException catch (e) {
       return DataFailed(e);
+    }
+  }
+
+  @override
+  Future<DataState<TypeBreedsModel>> getTypesAndBreeds(
+      TypeBreedsModel typeBreedsModel) async {
+    try {
+      final List<Map<String, dynamic>> queryParams = [
+        {
+          'name': {
+            'en': 'Horse',
+            'ru': 'Лошадь',
+            'kz': 'Жылкы',
+          },
+          'type': 1,
+          'breeds': [],
+        },
+        {
+          'name': {
+            'en': 'Cow',
+            'ru': 'Корова',
+            'kz': 'Сиыр',
+          },
+          'type': 2,
+          'breeds': [],
+        },
+      ];
+
+      final response = await _networkClient.getData(
+        endpoint: TypeBreedsEndpoint.GET_TYPE_AND_BREEDS.path,
+        queryParams: {'animals': queryParams},
+        parser: (response) => TypeBreedsModel.fromJson(response),
+      );
+
+      return DataSuccess(response);
+    } on CustomException catch (e) {
+      return DataFailed(e);
+    } catch (e) {
+      return DataFailed(
+        CustomException(message: 'Unexpected error occurred.'),
+      );
+    }
+  }
+
+  @override
+  Future<DataState<AdditionTypeModel>> getAdditionType(
+      AdditionTypeModel additionTypeModel) async {
+    try {
+      final List<Map<String, dynamic>> queryParams = [
+        {
+          "name": {
+            "ru": "Родился в хозяйстве",
+            "kz": "Шаруашылықта дүниеге келген",
+            "en": "Was born on the farm"
+          },
+          "type": 1
+        },
+        {
+          "name": {"ru": "Купленный", "kz": "Сатып алынған", "en": "Bought"},
+          "type": 2
+        },
+      ];
+
+      final response = await _networkClient.getData(
+        endpoint: AdditionTypeEndpoint.GET_ADDITION_TYPE.path,
+        queryParams: {'types': queryParams},
+        parser: (response) => AdditionTypeModel.fromJson(response),
+      );
+
+      return DataSuccess(response);
+    } on CustomException catch (e) {
+      return DataFailed(e);
+    } catch (e) {
+      return DataFailed(
+        CustomException(message: 'Unexpected error occurred.'),
+      );
     }
   }
 }
--- lib/features/livestock_list_page/domain/repositories/live_stock_repository.dart
+++ lib/features/livestock_list_page/domain/repositories/live_stock_repository.dart
@@ -1,7 +1,14 @@
 import 'package:malshy/core/utils/data_state.dart';
+import 'package:malshy/features/livestock_list_page/data/models/addition_type_model.dart';
+import 'package:malshy/features/livestock_list_page/data/models/type_breeds_model.dart';
 import '../../data/models/livestock_model.dart';
 
 abstract class LiveStockRepository {
   Future<DataState<LivestockModel>> createLiveStock(
       LivestockModel livestockModel);
+
+  Future<DataState<TypeBreedsModel>> getTypesAndBreeds(TypeBreedsModel typeBreedsModel);
+  Future<DataState<AdditionTypeModel>> getAdditionType(AdditionTypeModel additionTypeModel);
+
+
 }
